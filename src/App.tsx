9a1f1ce02cd976a802662d1202bbe2cd321081cf
jhbhan <43838586+jhbhan@users.noreply.github.com>
2026-04-14 20:47:36 -0700

    removed the secret page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { MyPage } from "./MyPage";
 import PlayGround from "./PlayGround";
-import { SecretPage } from "./SecretPage";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Route path="/" element={<MyPage />} />
         <Route path="/playground" element={<PlayGround />} />
         <Route path="/playground/:type" element={<PlayGround />} />
-        <Route path="/secret" element={<SecretPage />} />
         {/* wildcard route */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
